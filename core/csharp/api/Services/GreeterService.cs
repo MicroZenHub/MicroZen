81e81216bb36db91a5b97a59f25e337b4a38bdfc
ben.rinehart <ben@brinehartdesign.com>
2024-04-07 00:43:19 -0400

    Move folders

--- core/csharp/api/Services/GreeterService.cs
+++ core/csharp/api/Services/GreeterService.cs
@@ -9,6 +9,7 @@ public class GreeterService(ILogger<GreeterService> logger) : Greeter.GreeterBas
   /// <inheritdoc />
   public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
   {
+    logger.LogInformation("Saying hello to {Name}", request.Name);
     return Task.FromResult(new HelloReply
     {
         Message = "Hello " + request.Name
