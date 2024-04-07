2bc830dbf124a3599db269da32647e42d09d41f9
ben.rinehart <ben@brinehartdesign.com>
2024-04-06 23:10:58 -0400

    Reorganize apps

---
+++ core/C#/api-test/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace MicroZen.Core.Api.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ core/C#/api/Program.cs
@@ -0,0 +1,57 @@
+using Microsoft.OpenApi.Models;
+using MicroZen.Core.Api.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddGrpc().AddJsonTranscoding();
+builder.Services.AddGrpcHealthChecks();
+builder.Services.AddGrpcReflection();
+
+builder.Services.AddGrpcSwagger();
+builder.Services.AddSwaggerGen(c =>
+{
+  c.SwaggerDoc("v1",
+    new OpenApiInfo { Title = "MicroZen API", Version = "v1", Description = "The MicroZen API",  });
+
+  c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme {
+    In = ParameterLocation.Header,
+    Description = "Please enter into field the word 'Bearer' following by space and JWT access token",
+    Name = "Authorization",
+    Type = SecuritySchemeType.ApiKey,
+    Scheme = "Bearer"
+  });
+
+  c.AddSecurityRequirement(new OpenApiSecurityRequirement
+  {
+    {
+      new OpenApiSecurityScheme
+      {
+        Reference = new OpenApiReference
+        {
+          Type = ReferenceType.SecurityScheme,
+          Id = "Bearer"
+        },
+        Scheme = "oauth2",
+        Name = "Bearer",
+        In = ParameterLocation.Header
+      },
+      Array.Empty<string>()
+    }
+  });
+
+  var filePath = Path.Combine(System.AppContext.BaseDirectory, "MicroZen.Core.Api.xml");
+  c.IncludeXmlComments(filePath);
+  c.IncludeGrpcXmlComments(filePath, includeControllerXmlComments: true);
+});
+
+var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+  c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+});
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<GreeterService>();
+app.Run();
