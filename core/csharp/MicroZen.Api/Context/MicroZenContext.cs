3273a8bd7668057467931f13f88953da242ebe04
brinehart <ben.rinehart@avifoodsystems.comc>
2025-12-15 16:03:49 -0500

    Change default `ASPNETCORE_ENVIRONMENT` fallback from `Production` to `Development` in configuration builder

--- core/csharp/MicroZen.Api/Context/MicroZenContext.cs
+++ core/csharp/MicroZen.Api/Context/MicroZenContext.cs
@@ -66,7 +66,7 @@ public class MicroZenContext : DbContext
 	    new ConfigurationBuilder()
 		    .SetBasePath(Directory.GetCurrentDirectory())
 		    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-		    .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+		    .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development"}.json", optional: true)
 		    .AddEnvironmentVariables()
 		    .Build();
 }
